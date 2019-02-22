bbb24d07aa3496294b7a060e6e42fd4fe5a21123
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-02-22 22:46:26 +0530

    Add files via upload

---
+++ K-Closest-Point-To-The-Origin.cpp
@@ -0,0 +1,38 @@
+// Program to Find 'k' Closest Points to the Origin
+
+#include<bits/stdc++.h>
+using namespace std;
+
+double calDistance(int x, int y){
+
+	double z=sqrt(pow(abs(x),2)+pow(abs(y),2));
+	return z;
+}
+
+int main(){
+
+	vector <pair<int,int>> vp;
+	vp={{3,3},{5,-1},{-2,4}};
+	int k=2;
+
+	int n=vp.size();
+
+	typedef pair <double,int> pi;
+	priority_queue <pi,vector<pi>,greater<pi>> pq;
+
+	int x,y;
+	double z;
+
+	for(int i=0;i<n;i++){
+		x=vp[i].first;
+		y=vp[i].second;
+		z=calDistance(x,y);
+		pq.push(make_pair(z,i));
+	}
+	while(k--){
+		cout<<"("<<vp[pq.top().second].first<<","<<vp[pq.top().second].second<<")\n";
+		pq.pop();
+	}
+
+	return 0;
+}
